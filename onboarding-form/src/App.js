093b1d8fdea4220027e84fcc2fa57011965e5224
Matthew Arofin <Matt.arofin@gmail.com>
2022-05-25 16:23:21 -0400

    File scaffolding update

--- onboarding-form/src/App.js
+++ onboarding-form/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import {useState, useEffect} from 'react';
+import Form from './Components/Form';
 
 function App() {
+
+  // State const for formValues
+  // State const for Stored entries
+  // State const for errors
+  // State(?) const for validation
+  
+  /* 
+  - [ ] Name (first_name, last_name)
+  - [ ] Email
+  - [ ] Password
+  - [ ] Terms of Service (checkbox)
+  - [ ] A Submit button to send our form data to the server.
+  */
+
   return (
     <div className="App">
-      
+      <h1>This App is Up and Running 🚀</h1>
+      <h3></h3>
+      <Form />
     </div>
   );
 }
--- onboarding-form/src/Components/Form.js
+++ onboarding-form/src/Components/Form.js
@@ -1,1 +1,22 @@
 // 
+
+export default function Form(props) {
+    const {} = props;
+
+    /* 
+  - [ ] Name (first_name, last_name)
+  - [ ] Email
+  - [ ] Password
+  - [ ] Terms of Service (checkbox)
+  - [ ] A Submit button to send our form data to the server.
+  */
+
+  return (
+      <form>
+          <div>
+              
+          </div>
+      </form>
+  )
+
+}
