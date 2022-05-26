ef63597b835b5d28cebb27377ecf95a0300c72f7
Matthew Arofin <Matt.arofin@gmail.com>
2022-05-25 20:02:34 -0400

    Form scaffolding update

--- onboarding-form/src/App.js
+++ onboarding-form/src/App.js
@@ -1,27 +1,85 @@
 import './App.css';
 import {useState, useEffect} from 'react';
 import Form from './Components/Form';
+import axios from 'axios';
+import * as yup from 'yup';
+import schema from './Validation/formSchema';
+
+/* 
+Post Endpoint URL: https://reqres.in/api/users
+Get Endpoint URL: 
+*/
+
+const initialFormValues  = {
+  name: '', // text input
+  email: '', // text input
+  password: '', // text input
+  terms: false, // checkbox
+};
+
+const initialFormErrors = {
+  username: '',
+  email: '',
+  password: '',
+}
+
+const initialMembers = [];
+const initialDisabled = true;
 
 function App() {
 
-  // State const for formValues
-  // State const for Stored entries
-  // State const for errors
-  // State(?) const for validation
-  
-  /* 
-  - [ ] Name (first_name, last_name)
-  - [ ] Email
-  - [ ] Password
-  - [ ] Terms of Service (checkbox)
-  - [ ] A Submit button to send our form data to the server.
-  */
+  const [members, setMembers] = useState(initialMembers);
+  const [formValues, setFormValues] = useState(initialFormErrors);  // State const for Stored entries
+  const [formErrors, setFormErrors] = useState(initialFormErrors); // State const for errors
+  const [disabled, setDisabled] = useState(initialDisabled); // State(?) const for validation
+
+  // Helpers
+  const getMembers = () => {
+
+  };
+
+  // Event handlers
+  const inputChange = (name, value) => {
+    setFormValues({...formValues, [name]: value})
+  };
+
+  const formSubmit = () => {
+    const newMember = {
+      fname: formValues.fname.trim(),
+      lname: formValues.lname.trim(),
+      email: formValues.email.trim(),
+      password: formValues.password,
+      terms: formValues.terms
+    }
+
+    postNewMember(newMember);
+  };  
+
+  const postNewMember = newMember => {
+    axios.post('https://reqres.in/api/users', newMember)
+      .then(res => {
+        console.log(res)
+      }).catch(err => {
+        console.error(err)
+      }).finally(() => {
+        setFormValues(initialFormValues);
+      })
+  }
 
   return (
     <div className="App">
       <h1>This App is Up and Running 🚀</h1>
-      <h3></h3>
-      <Form />
+      <Form 
+        values={formValues}
+        change={inputChange}
+        submit={formSubmit}
+        disabled={disabled}
+        errors={formErrors}
+      />
+
+      {/* {
+        members.map(() => {};)
+      } */}
     </div>
   );
 }
--- onboarding-form/src/Components/Form.js
+++ onboarding-form/src/Components/Form.js
@@ -1,8 +1,10 @@
 // 
 
-export default function Form(props) {
-    const {} = props;
 
+
+export default function Form(props) {
+    const { values, change, submit, disabled, errors} = props;
+        
     /* 
   - [ ] Name (first_name, last_name)
   - [ ] Email
@@ -11,10 +13,37 @@ export default function Form(props) {
   - [ ] A Submit button to send our form data to the server.
   */
 
+  const onChange = evt => {};
+
+  const onSubmit = evt => {
+      evt.preventDefault();
+      submit()
+  };
+
   return (
-      <form>
-          <div>
-              
+      <form className="form-container" onSubmit={onSubmit}>
+          <div className="form-group submit">
+                <h2>Add New User</h2>
+
+                <div className="errors">
+                    <div>{errors.fname}</div>
+                    <div>{errors.fname}</div>
+                    <div>{errors.fname}</div>
+                    <div>{errors.fname}</div>
+                    <div>{errors.fname}</div>
+
+                </div>
+
+                <div className="form-group submit">
+
+                </div>
+
+                <button disabled={disabled}>submit</button>
+                    <label>First Name
+                        <input 
+
+                        />
+                    </label>
           </div>
       </form>
   )
